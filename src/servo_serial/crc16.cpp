cf9119295508c534f326d817fbb6417453df0c5b
akiyoshi <uchida.akiyoshi.s3@dc.tohoku.ac.jp>
2025-03-10 14:42:26 +0100

    Add crc16.cpp source file to servo_serial and include cstdint header

--- src/servo_serial/crc16.cpp
+++ src/servo_serial/crc16.cpp
@@ -20,6 +20,8 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 
+#include <cstdint>
+
 #include "t_motor_hardware_interface/servo_serial/crc16.hpp"
 
 namespace t_motor_hardware_interface {
