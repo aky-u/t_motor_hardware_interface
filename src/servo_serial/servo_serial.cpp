2b5f6cbaefc154b871b5d7b9d3290545552e22b9
akiyoshi <uchida.akiyoshi.s3@dc.tohoku.ac.jp>
2025-03-10 16:45:14 +0100

    Remove redundant temperature parsing from parseMotorParameters in ServoSerial class

--- src/servo_serial/servo_serial.cpp
+++ src/servo_serial/servo_serial.cpp
@@ -50,9 +50,6 @@ MotorState ServoSerial::parseMotorParameters(const std::vector<uint8_t> &data) c
   MotorState motor_state;
   // Parse the data and fill in the motor state object
 
-  float mos_temperature = (data[4] << 8 | data[5]) / 100.0;
-  motor_state.setMosTemperature(mos_temperature);
-
   return motor_state;
 }
 
